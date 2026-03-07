ed70a9511c49eae23cad1b10c07c4c18b32187d8
Hoang Minh Tuan Nguyen <108424069+mtuannh@users.noreply.github.com>
2026-03-08 00:40:19 +1100

    add photos to the homepage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-âsf
+function showPage(pageId) {
+    //to hide pages
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => page.classList.remove('active'));
+
+    //to show selected page
+    const activePage = document.getElementById(pageId);
+    if (activePage) {
+        activePage.classList.add('active');
+    }
+}
+
+//defaultly show home page
+window.onload = () => showPage('home');
